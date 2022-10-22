819f39a97d32656c769ef7a60ffc84d05a1e48f1
Obi-Codez <obinnaogbu80@gmail.com>
2022-10-22 14:41:38 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,27 +1,14 @@
 #include <stdio.h>
-
-
-
 /**
+ * main - Entry Point
+ * 
+ * Description: using puts function to give an output
  *
- *  * main - Entry Point
- *
- *   *
- *
- *    * Description: using puts function to give an output
- *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- *       */
+ * Return: Allways 0 (success)
+ */
 
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }
