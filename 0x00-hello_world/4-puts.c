87571d12fccd4d6391d3e6b0421d6f3c96cddecb
Obi-Codez <obinnaogbu80@gmail.com>
2022-10-22 14:09:27 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: using puts function to give an output
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
